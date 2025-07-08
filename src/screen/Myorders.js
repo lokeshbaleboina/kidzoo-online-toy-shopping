08a21a228231d9d0cade37961586ca266e0af8a6
lokeshbaleboina <lokeshbaleboina@gmail.com>
2025-07-08 10:54:02 +0530

    Myorders.js

--- src/screen/Myorders.js
+++ src/screen/Myorders.js
@@ -8,7 +8,7 @@ export default function Myorder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("https://kidzoo-online-toy-shopping-backend.onrender.com/api/myOrderData", {
           
             method: 'POST',
             headers: {
