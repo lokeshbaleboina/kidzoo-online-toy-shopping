219eef16bc49072314879b2b1aea79e0304100cb
lokeshbaleboina <lokeshbaleboina@gmail.com>
2025-07-08 10:52:27 +0530

    Cart.js

--- src/screen/Cart.js
+++ src/screen/Cart.js
@@ -15,7 +15,7 @@ export default function Cart() {
 
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://kidzoo-online-toy-shopping-backend.onrender.com/api/orderData", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
