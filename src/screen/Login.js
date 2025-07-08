c2ca941737a4ad54076c9064962a67148aad79a5
lokeshbaleboina <lokeshbaleboina@gmail.com>
2025-07-08 10:53:34 +0530

    Login.js

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -6,7 +6,7 @@ export default function Login() {
   let navigate = useNavigate()
   const handlesubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://kidzoo-online-toy-shopping-backend.onrender.com/api/loginuser", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
